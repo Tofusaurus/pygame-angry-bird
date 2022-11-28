7c182577b15b9922e2fb29b40e366417c7fc4770
Tofusaurus <fzhu9@My.bcit.ca>
2022-11-27 18:33:17 -0800

    created objects slab class

--- objects.py
+++ objects.py
@@ -4,3 +4,36 @@ from math import *
 
 import physics_engine
 
+pygame.init()
+display = None
+width = None
+height = None
+clock = pygame.time.Clock()
+ground = 50
+
+def init(screen):
+    global width, height, display
+    display = screen
+    (width, height) = display.get_rect().size
+    height -= ground
+
+class Slab:
+    def __init__(self, x, y, w, h, color=(255, 255, 255)):
+        self.x = x
+        self.y = y
+        self.w = w
+        self.h = h
+
+        if self.w > self.h:
+            self.image = pygame.image.load("Images/wall_horizontal.png").convert_alpha()
+        else:
+            self.image = pygame.image.load("Images/wall_vertical.png").convert_alpha()
+
+        self.image = pygame.transform.scale(self.image, (self.w, self.h))
+
+        self.color = color
+
+    def draw(self):
+        display.blit(self.image, (self.x, self.y))
+
+    
