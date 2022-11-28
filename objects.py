5b9b1dfd3c53bd3926a7729f4909b2286035ffd0
Tofusaurus <fzhu9@My.bcit.ca>
2022-11-27 18:33:50 -0800

    setting up the collision manager to deal with different collisions

--- objects.py
+++ objects.py
@@ -36,4 +36,51 @@ class Slab:
     def draw(self):
         display.blit(self.image, (self.x, self.y))
 
-    
+    def collision_manager(self, ball, type="BALL"):
+        if type == "BALL":
+            if (ball.y + ball.r > self.y) and (ball.y < self.y + self.h):
+                if (ball.x < self.x + self.w) and (ball.x + ball.r > self.x + self.w):
+                    ball.x = 2*(self.x + self.w) - ball.x
+                    ball.velocity.angle = - ball.velocity.angle
+                    ball.velocity.magnitude *= physics_engine.elasticity
+                elif ball.x + ball.r > self.x and (ball.x < self.x):
+                    ball.x = 2*(self.x - ball.r) - ball.x
+                    ball.velocity.angle = - ball.velocity.angle
+                    ball.velocity.magnitude *= physics_engine.elasticity
+            if (ball.x + ball.r > self.x) and (ball.x < self.x + self.w):
+                if ball.y + ball.r > self.y and ball.y < self.y:
+                    ball.y = 2*(self.y - ball.r) - ball.y
+                    ball.velocity.angle = pi - ball.velocity.angle
+                    ball.velocity.magnitude *= physics_engine.elasticity
+                elif (ball.y < self.y + self.h) and (ball.y + ball.r > self.y + self.h):
+                    ball.y = 2*(self.y + self.h) - ball.y
+                    ball.velocity.angle = pi - ball.velocity.angle
+                    ball.velocity.magnitude *= physics_engine.elasticity
+
+            return ball
+        else:
+            block = ball
+            if (block.y + block.h > self.y) and (block.y < self.y + self.h):
+                if (block.x < self.x + self.w) and (block.x + block.w > self.x + self.w):
+                    block.x = 2*(self.x + self.w) - block.x
+                    block.velocity.angle = - block.velocity.angle
+                    block.rotateAngle =  - block.velocity.angle
+                    block.velocity.magnitude *= physics_engine.elasticity
+                elif block.x + block.w > self.x and (block.x < self.x):
+                    block.x = 2*(self.x - block.w) - block.x
+                    block.velocity.angle = - block.velocity.angle
+                    block.rotateAngle =  - block.velocity.angle
+                    block.velocity.magnitude *= physics_engine.elasticity
+            if (block.x + block.w > self.x) and (block.x < self.x + self.w):
+                if block.y + block.h > self.y and block.y < self.y:
+                    block.y = 2*(self.y - block.h) - block.y
+                    block.velocity.angle = pi - block.velocity.angle
+                    block.rotateAngle =  pi - block.velocity.angle
+                    block.velocity.magnitude *= physics_engine.elasticity
+                elif (block.y < self.y + self.h) and (block.y + block.h > self.y + self.h):
+                    block.y = 2*(self.y + self.h) - block.y
+                    block.velocity.angle = pi - block.velocity.angle
+                    block.rotateAngle =  pi - block.velocity.angle
+                    block.velocity.magnitude *= physics_engine.elasticity
+
+            return block
